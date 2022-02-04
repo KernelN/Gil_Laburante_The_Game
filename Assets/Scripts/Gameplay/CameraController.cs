40c016f0d5de3e8d529bb2e4c79a798387a7022b
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-02-04 19:36:25 -0300

    Add CameraController

---
+++ Assets/Scripts/Gameplay/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace GilLaburante.Gameplay
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform target;
+        [SerializeField]
+        private Vector3 targetOffset;
+        [SerializeField]
+        private float movementSpeed;
+
+        void Update()
+        {
+            MoveCamera();
+        }
+
+        void MoveCamera()
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position + targetOffset, movementSpeed * Time.deltaTime);
+        }
+    }
+}
