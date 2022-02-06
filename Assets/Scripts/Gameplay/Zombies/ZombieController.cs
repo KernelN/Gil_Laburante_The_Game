00a5294d20b8ba66caa75e4e1b524f9eb526d10b
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-02-06 20:21:57 -0300

    Add zombie movement

---
+++ Assets/Scripts/Gameplay/Zombies/ZombieController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace GilLaburante.Gameplay.Zombies
+{
+	public class ZombieController : MonoBehaviour, IHittable
+	{
+		public ZombieData publicData { get { return data; } }
+
+		[Header("Set Values")]
+		[SerializeField] NavMeshAgent navMesh;
+		[SerializeField] ZombieData data;
+
+		[Header("Runtime Values")]
+		[SerializeField] Transform target;
+
+        //Unity Events
+        private void Start()
+        {
+            data.currentStats = data.baseStats;
+            if (navMesh == null)
+            {
+                navMesh = transform.GetComponent<NavMeshAgent>();
+            }
+        }
+		void Update()
+        {
+			Collider[] targetsHitted;
+            targetsHitted = Physics.OverlapSphere(transform.position, data.detectRange, data.targetLayers);
+
+            if (targetsHitted.Length > 0)
+            {
+                if (target != targetsHitted[0].transform)
+                {
+                    target = targetsHitted[0].transform;
+                }
+
+                navMesh.SetDestination(target.position);
+				UpdateTarget();
+			}
+            else if (target)
+            {
+				target = null;
+            }
+        }
+
+        //Methods
+		void UpdateTarget()
+        {
+            //if (target)
+            //{
+            //	navMesh.enabled = true;
+            if (Physics.Raycast(transform.position, target.position - transform.position))
+            {
+                transform.LookAt(target);
+                navMesh.SetDestination(target.position);
+            }
+            //}
+            //else if (navMesh.enabled)
+            //	navMesh.enabled = false;
+        }
+
+        //Interface Implemantation
+        public void GetHitted(int damage)
+		{
+			data.currentStats.health -= damage;
+			if (data.currentStats.health <= 0)
+				Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Gameplay/Zombies/ZombieData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GilLaburante.Gameplay.Zombies
+{
+    [System.Serializable]
+	public struct ZombieData
+    {
+        [Header("Set Values")]
+        public Stats baseStats;
+        public LayerMask targetLayers;
+        public float detectRange;
+        [Header("Current Values")]
+        public Stats currentStats;
+    }
+}
