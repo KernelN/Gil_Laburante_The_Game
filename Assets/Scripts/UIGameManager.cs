cdf052b868734598214972dab2a5ac4f13fe7ea5
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-02-09 17:00:07 -0300

    merge

---
+++ Assets/Scripts/UIGameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UIGameManager : MonoBehaviour
+{
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.Get();
+    }
+
+    public void LoadScene(int sceneEnumNumber)
+    {
+        gameManager.LoadScene(sceneEnumNumber);
+    }
+    public void QuitGame()
+    {
+        gameManager.QuitGame();
+    }
+}
