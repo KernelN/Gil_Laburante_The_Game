30461bd1090ac44e7fbd8fb4872f6a0ae309f542
Alejandro <alejandroninsausti@gmail.com>
2022-02-09 12:14:15 -0300

    Fix Joysticks freezing in position

--- Assets/Scripts/Gameplay/Inputs/VirtualJoystick.cs
+++ Assets/Scripts/Gameplay/Inputs/VirtualJoystick.cs
@@ -4,82 +4,89 @@ using UnityEngine.EventSystems;
 
 namespace GilLaburante.Gameplay.Inputs
 {
-    public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerExitHandler, IPointerUpHandler
+    public class VirtualJoystick : VariableJoystick//, IDragHandler, IPointerExitHandler, IPointerUpHandler
     {
         public Action<Vector2> InputRecieved;
-        [SerializeField] RectTransform joystickHolder;
-        [SerializeField] RectTransform joystick;
         [SerializeField] internal InputManager inputManager;
-        bool pointerOver;
 
         //Unity Events        
         private void Update()
         {
-            if (pointerOver)
-                InputRecieved?.Invoke(GetAxisOutput());
+            if (Mathf.Abs(Horizontal) > 0 || Mathf.Abs(Vertical) > 0)
+                InputRecieved?.Invoke(Direction);
         }
+
+        #region DEPRECATED
+        //[SerializeField] RectTransform joystickHolder;
+        //[SerializeField] RectTransform joystick;
+        //bool pointerOver;
 
         //Methods
-        void MoveStick(Vector2 pointerPosition)
-        {
-            Vector2 newPosition;
-            newPosition.x = GetStickPosX(pointerPosition.x - joystickHolder.position.x);
-            newPosition.y = GetStickPosY(pointerPosition.y - joystickHolder.position.y);
-            //joystick.anchoredPosition = newPosition;
-            joystick.localPosition = newPosition;
-        }
-        float GetStickPosX(float x)
-        {
-            if (Mathf.Abs(x) < joystickHolder.sizeDelta.x / 2)
-            {
-                return x;
-            }
+        //void MoveStick(Vector2 pointerPosition)
+        //{
+        //    Vector2 newPosition;
+        //    newPosition.x = GetStickPosX(pointerPosition.x - joystickHolder.position.x);
+        //    newPosition.y = GetStickPosY(pointerPosition.y - joystickHolder.position.y);
+        //    //joystick.anchoredPosition = newPosition;
+        //    joystick.localPosition = newPosition;
+        //}
+        //float GetStickPosX(float x)
+        //{
+        //    if (Mathf.Abs(x) < joystickHolder.sizeDelta.x / 2)
+        //    {
+        //        return x;
+        //    }
 
-            if (x > joystickHolder.sizeDelta.x / 2)
-            {
-                return joystickHolder.sizeDelta.x / 2 - joystickHolder.sizeDelta.x / 10;
-            }
-            else
-            {
-                return -joystickHolder.sizeDelta.x / 2 + joystickHolder.sizeDelta.x / 10;
-            }
-        }
-        float GetStickPosY(float y)
-        {
-            if (Mathf.Abs(y) < joystickHolder.sizeDelta.y / 2)
-            {
-                return y;
-            }
+        //    if (x > joystickHolder.sizeDelta.x / 2)
+        //    {
+        //        return joystickHolder.sizeDelta.x / 2 - joystickHolder.sizeDelta.x / 10;
+        //    }
+        //    else
+        //    {
+        //        return -joystickHolder.sizeDelta.x / 2 + joystickHolder.sizeDelta.x / 10;
+        //    }
+        //}
+        //float GetStickPosY(float y)
+        //{
+        //    if (Mathf.Abs(y) < joystickHolder.sizeDelta.y / 2)
+        //    {
+        //        return y;
+        //    }
 
-            if (y > joystickHolder.sizeDelta.y / 2)
-            {
-                return joystickHolder.sizeDelta.y / 2 - joystickHolder.sizeDelta.x / 10;
-            }
-            else
-            {
-                return -joystickHolder.sizeDelta.y / 2 + joystickHolder.sizeDelta.x / 10;
-            }
-        }
-        Vector2 GetAxisOutput()
-        {
-            return joystick.anchoredPosition.normalized;
-        }
+        //    if (y > joystickHolder.sizeDelta.y / 2)
+        //    {
+        //        return joystickHolder.sizeDelta.y / 2 - joystickHolder.sizeDelta.x / 10;
+        //    }
+        //    else
+        //    {
+        //        return -joystickHolder.sizeDelta.y / 2 + joystickHolder.sizeDelta.x / 10;
+        //    }
+        //}
+        //Vector2 GetAxisOutput()
+        //{
+        //    return joystick.anchoredPosition.normalized;
+        //}
+        //void ReleaseStick()
+        //{
+        //    pointerOver = false;
+        //    joystick.anchoredPosition *= 0; //reset pos
+        //    joystick.localPosition *= 0; //reset pos
+        //}
 
         //Interface Implementations        
-        public void OnDrag(PointerEventData eventData)
-        {
-            MoveStick(eventData.position);
-            pointerOver = true;
-        }
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            pointerOver = false;
-            joystick.anchoredPosition *= 0; //reset pos
-        }
-        public void OnPointerUp(PointerEventData eventData)
-        {
-            pointerOver = false;
-            joystick.anchoredPosition *= 0; //reset pos
-        }
+        //public void OnDrag(PointerEventData eventData)
+        //{
+        //    MoveStick(eventData.position);
+        //    pointerOver = true;
+        //}
+        //public void OnPointerExit(PointerEventData eventData)
+        //{
+        //    ReleaseStick();
+        //}
+        //public void OnPointerUp(PointerEventData eventData)
+        //{
+        //    ReleaseStick();
+        //}
+        #endregion
     }
 }
